4d57c77c3caad39a0a67e45ce4d16d0881eb6574
baturgezici <batur.gezici@gmail.com>
2023-03-26 06:41:12 +0300

    #FINAL Pages added and minor issues fixed

--- esdera/cablecomp/views.py
+++ esdera/cablecomp/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
+import sqlite3
+import pandas as pd
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'cablecomp/index.html')
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, 'cablecomp/about.html')
 
 def cables(request):
-    return render(request, 'cables.html')
+
+    cnx = sqlite3.connect('db.sqlite3')
+    df = pd.read_sql_query("SELECT * FROM data", cnx, index_col='id')
+    table_dict = df.to_dict('records')
+
+    return render(request, 'cablecomp/cables.html', {'table_dict': table_dict})
